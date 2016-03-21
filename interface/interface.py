27dda9cf6a9c7f6e1dd337862a82a1b6321d7d7f
Ricardo Rodriguez <rr.ricci@gmail.com>
2016-03-21 11:28:36 +0100

    Début codification interface

--- interface/interface.py
+++ interface/interface.py
@@ -3,10 +3,13 @@
 
 #############################################################################
 #    Interface :                                                            #
-#        Module pour .....                                                  #
-#                                                                           #
-#                                                                           #
-#                                                                           #
+#        Cette classe implémente une interface de visualisation en ligne    #
+#        de commande, qui affichera pour chaque serveur le dernier état     #
+#        connu, un graphe d’évolution pour les données aillant un           #
+#        historique, ainsi que la date depuis la dernière communication.    #
+#        Ce package intègre aussi des éléments de contrôle lui permettant   #
+#        de détecter une situation de crise et de prévenir l’administrateur #
+#        par e-mail.                                                        #
 #                                                                           #
 #############################################################################
 
