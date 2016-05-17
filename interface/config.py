7d4f3aab56603982aa80c27c6e815e25fadec0f1
Ricardo Rodríguez <rr.ricci@gmail.com>
2016-05-17 19:02:38 +0200

    Interface presque fini

---
+++ interface/config.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#############################################################################
+#    Config :                                                               #
+#        Classe pour gérer la configuration de l'application.               #
+#                                                                           #
+#############################################################################
+
+from lxml import etree as ET
+
+
+#############################################################################
+#    Config.                                                                #
+#############################################################################
+
+class Config:
+    PATH_CONFIG = "config.xml"
+
+    def __init__(self):
+        f = open(self.PATH_CONFIG, "r")
+        self.config_root = ET.fromstring(f.read())
+        f.close()
+
+    def save(self):
+        self.indent_xml(self.config_root)
+        content = '<?xml version="1.0" encoding="UTF-8"?>\n' \
+                    + ET.tostring(self.config_root)
+        f = open(self.PATH_CONFIG, "w")
+        f.write(content)
+        f.close()
+
+    def indent_xml(self, elem, level=0):
+        i = "\n" + level*"    "
+        if len(elem):
+            if not elem.text or not elem.text.strip():
+                elem.text = i + "    "
+            if not elem.tail or not elem.tail.strip():
+                elem.tail = i
+            for elem in elem:
+                self.indent_xml(elem, level+1)
+            if not elem.tail or not elem.tail.strip():
+                elem.tail = i
+        else:
+            if level and (not elem.tail or not elem.tail.strip()):
+                elem.tail = i
+
+    def get(self, key):
+        return self.config_root.find(key).text
+
+    def set(self, key, value):
+        self.config_root.find(key).text = value
